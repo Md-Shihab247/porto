eab54be4606f60f86d4868bbe529407c4583c92e
Md-Shihab247 <sheikhmdsihab1@gmail.com>
2025-04-05 22:11:00 +0600

    test purpose
    font icon disable

--- src/components/Awesome.jsx
+++ src/components/Awesome.jsx
@@ -8,14 +8,14 @@ const Awesome = () => {
                 <div className="text-center">
                     <h2 className="text-[#212529] text-[42px] font-bold leading-[42px] mb-[32px]">We are Porto, we are Awesome</h2>
                     <p className="text-[#777] leading-[1.9] font-normal text-[16.8px] max-w-[810px] mx-auto mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras volutpat id sapien ac varius. Fusce hendrerit ligula a consectetur ullamcorper. Vestibulum varius pharetra lorem, in maximus libero placerat sed. In a lectus vel mauris tempor lobortis feugiat sed magna.</p>
-                </div>
+                </div> 
 
-                <div className="flex items-center justify-between ">
+                {/* <div className="flex items-center justify-between ">
                     <Planing heading="Strategy" icon={<i class="fa-regular fa-comments style"></i>}/>
                     <Planing heading="Plan Everything" icon={<i class="fa-solid fa-sitemap style"></i>}/>
                     <Planing heading="Work Hard" icon={<i class="fa-solid fa-mug-hot style"></i>}/>
                     <Planing heading="Deliver Quality" icon={<i class="fa-regular fa-heart style"></i>}/>
-                </div>
+                </div> */}
             </div>
         </section>
     </>
